022f55a02a6e7398808a25365dd64d76a6f7e9ea
moriryota1218 <mryota5618@gmail.com>
2020-01-03 16:38:52 +0900

    company_infoページの作成

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ $(function(){
   var pagetop = $('#page_top');
   // ボタン非表示
   pagetop.hide();
-  // 300px スクロールしたらボタン表示
+  // 200px スクロールしたらボタン表示
   $(window).scroll(function () {
-     if ($(this).scrollTop() > 300) {
+     if ($(this).scrollTop() > 200) {
           pagetop.fadeIn();
      } else {
           pagetop.fadeOut();
